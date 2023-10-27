d5d935f734280d6b91b6224afc5446c91571f2d2
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-10-27 17:13:43 +0200

    add hover effect to the film cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,20 +11,26 @@
 
     <div class="row my-4">
         @foreach($movies as $movie)
-        <div class="col-4 gy-4">
-            <div class="card h-100">
+        <div class="film col-4 gy-4">
+            <div class=" film-front card h-100">
                 <div class="card-header h-25">
                     <h4>{{$movie->title}}</h4>
                     <small>{{$movie->original_title}}</small>
                 </div>
                 <div class="card-body">
-                    <img class="img-fluid" src="https://picsum.photos/200/300?random=<?= rand() ?>">
-                    <span>release: {{$movie->date}}</span>
+                    <img class="img-fluid" src="https://picsum.photos/200/300?random={{$movie->id}}">
                 </div>
-                <div class="card-footer">
+            </div>
 
-                    <p>nation: {{$movie->nationality}}</p>
-                    <p>vote: {{$movie->vote}}</p>
+            <div class=" film-back card h-100">
+                <div class="card-header">
+                    <h6 class="m-0">{{$movie->title}}</h6>
+                    <small class="m-0">vote: {{$movie->vote}}</small>
+                </div>
+                <div class="card-body">
+                    <span class="m-0">nation: {{$movie->nationality}}</span>
+                    <img class="img-fluid" src="https://picsum.photos/200/300?random={{$movie->id}}">
+                    <span>release: {{$movie->date}}</span>
                 </div>
             </div>
         </div>
